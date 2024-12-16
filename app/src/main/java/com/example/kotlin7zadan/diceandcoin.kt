b6a8de5ad32818f60621b4bc08d4959fc7815699
ignacy <adsa@asda.com>
2024-12-16 11:30:49 +0100

    initialize repository

---
+++ app/src/main/java/com/example/kotlin7zadan/diceandcoin.kt
@@ -0,0 +1,35 @@
+package com.example.kotlin7zadan
+import kotlin.random.Random
+
+interface RollingTool {
+    fun roll()
+}
+
+class Coin : RollingTool {
+    override fun roll() {
+        val randomInt = Random.nextInt(0, 2)
+        if(randomInt == 1) {
+            println("Heads")
+        }
+        else {
+            println("Tails")
+        }
+    }
+}
+
+class Dice : RollingTool {
+    override fun roll() {
+        val randomInt = Random.nextInt(1,7)
+        println(randomInt)
+    }
+}
+
+
+fun main() {
+    val coin = Coin()
+    val dice = Dice()
+
+    coin.roll()
+    dice.roll()
+
+}
