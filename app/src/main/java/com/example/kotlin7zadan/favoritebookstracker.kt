040d1b3367fef7c2e5140cf2d14dad4e58b73464
kubarodziewicz <kuba.rodziewicz@cyfroweszkoly.pl>
2024-12-21 22:27:02 +0100

    change singleton favoritebookstracker to data class Book and implement a list of books

--- app/src/main/java/com/example/kotlin7zadan/favoritebookstracker.kt
+++ app/src/main/java/com/example/kotlin7zadan/favoritebookstracker.kt
@@ -1,5 +1,20 @@
 package com.example.kotlin7zadan
 
-object FavoriteBooksTracker {
+data class Book(val title: String, val author: String, val year: Int)
+
+fun main() {
+    val books = listOf(
+        Book("The Midnight Library", "Matt Haig", 2020),
+        Book("Where the Crawdads Sing", "Delia Owens", 2018),
+        Book("Dune", "Frank Herbert", 1965),
+        Book("1984", "George Orwell", 1949),
+        Book("To Kill a Mockingbird", "Harper Lee", 1960),
+        Book("The Alchemist", "Paulo Coelho", 1988),
+        Book("The Catcher in the Rye", "J.D. Salinger", 1951),
+        Book("Brave New World", "Aldous Huxley", 1932),
+        Book("Sapiens: A Brief History of Humankind", "Yuval Noah Harari", 2011),
+        Book("Atomic Habits", "James Clear", 2018)
+    )
 
 }
+
