24e149f6ff8afb19b80a98a979024c4f3ec34549
kubarodziewicz <kuba.rodziewicz@cyfroweszkoly.pl>
2024-12-22 13:25:49 +0100

    implement classes for certain types of weather in weatherapp.kt

--- app/src/main/java/com/example/kotlin7zadan/weatherapp.kt
+++ app/src/main/java/com/example/kotlin7zadan/weatherapp.kt
@@ -5,5 +5,35 @@ interface Weather {
 }
 
 
+class Sunny: Weather {
+    override fun displayWeather() {
+        println("Weather is sunny")
+    }
+}
+
+class Rainy: Weather {
+    override fun displayWeather() {
+        println("Weather is rainy")
+    }
+}
+
+class Stormy: Weather {
+    override fun displayWeather() {
+        println("Weather is stormy")
+    }
+}
+
+class Snowy: Weather {
+    override fun displayWeather() {
+        println("Weather is snowy")
+    }
+}
+
+class Foggy: Weather {
+    override fun displayWeather() {
+        println("Weather is foggy")
+    }
+}
 
 data class City(val name: String, val weather: Weather)
+
