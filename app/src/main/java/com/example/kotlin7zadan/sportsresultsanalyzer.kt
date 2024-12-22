b442ebffbb9d57835f73aa8f2e84cfc91941ac5f
ignacybanet <ignacybanet@cyfroweszkoly.pl>
2024-12-22 22:13:10 +0100

    create match list, create match adding functionality, create companion object, reformat code

--- app/src/main/java/com/example/kotlin7zadan/sportsresultsanalyzer.kt
+++ app/src/main/java/com/example/kotlin7zadan/sportsresultsanalyzer.kt
@@ -1,7 +1,26 @@
 package com.example.kotlin7zadan
 
-class Analyzer(val matchName: String, var matchPoints: Int) {
-    fun addMatch(userMatchName: String, userMatchPoints: Int) {
-       val teamMatches: Map<String,Int> = mutableMapOf("match1" to 3, "match2" to 7, "match3" to 4, "match4" to 8, "match5" to 2, "match6" to 5, "match7" to 9, "match8" to 6, "match9" to 1, "match10" to 10)
+class Analyzer {
+    companion object {
+        var teamMatches: Map<String, Int> = mutableMapOf(
+            "match1" to 3,
+            "match2" to 7,
+            "match3" to 4,
+            "match4" to 8,
+            "match5" to 2,
+            "match6" to 5,
+            "match7" to 9,
+            "match8" to 6,
+            "match9" to 1,
+            "match10" to 10
+        )
+
+        fun addMatch(userMatchName: String, userMatchPoints: Int) {
+            teamMatches += userMatchName to userMatchPoints
+        }
     }
+}
+
+fun main() {
+    println(Analyzer.teamMatches)
 }
