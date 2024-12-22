a380d86c5c169dd4645f980d0a49782dad07f52a
ignacybanet <ignacybanet@cyfroweszkoly.pl>
2024-12-22 22:01:58 +0100

    create Analyzer class, addMatch method, and teamMatches map

--- app/src/main/java/com/example/kotlin7zadan/sportsresultsanalyzer.kt
+++ app/src/main/java/com/example/kotlin7zadan/sportsresultsanalyzer.kt
@@ -1,1 +1,7 @@
 package com.example.kotlin7zadan
+
+class Analyzer(val matchName: String, var matchPoints: Int) {
+    fun addMatch(userMatchName: String, userMatchPoints: Int) {
+       val teamMatches: Map<String,Int> = mutableMapOf("match1" to 3, "match2" to 7, "match3" to 4, "match4" to 8, "match5" to 2, "match6" to 5, "match7" to 9, "match8" to 6, "match9" to 1, "match10" to 10)
+    }
+}
